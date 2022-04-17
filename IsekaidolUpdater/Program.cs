4c2c13b452e92af1176c092550d6c3d09968483e
NeuroWhAI <tlsehdgus0212@gmail.com>
2022-04-17 17:20:53 +0900

    first commit

---
+++ IsekaidolUpdater/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace IsekaidolUpdater
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("url interval");
+                return;
+            }
+
+            string url = args[0];
+            var interval = TimeSpan.FromMilliseconds(int.Parse(args[1]));
+
+            Console.WriteLine("URL : {0}, Interval : {1}", url, interval.TotalMilliseconds);
+
+            while (true)
+            {
+                try
+                {
+                    var start = DateTime.UtcNow;
+                    var res = await client.GetAsync(url);
+                    Console.WriteLine("Status : {0}", res.StatusCode);
+                    if (res.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        string content = await res.Content.ReadAsStringAsync();
+                        Console.WriteLine("Content : {0}", content);
+                    }
+                    while (DateTime.UtcNow - start < interval)
+                    {
+                        await Task.Delay(10);
+                    }
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine(err.Message);
+                    Console.WriteLine(err.StackTrace);
+
+                    await Task.Delay(4000);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Isekaidol Updater
+
+[이세돌 방송 알림 프로젝트](https://github.com/NeuroWhAI/isekaidol-stream-noti)를 위한 작업 요청 프로그램.
